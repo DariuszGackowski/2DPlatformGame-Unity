ea65438d812bfe7401da8aa6da26b79f33ad3da0
DariuszGackowski <dariuszgackowski@wp.pl>
2021-05-31 21:16:38 +0200

    Update 0.1

--- PlatformGameDemo/Assets/Scripts/MenuAndPanels/Menu/GemMarket/Drawing.cs
+++ PlatformGameDemo/Assets/Scripts/MenuAndPanels/Menu/GemMarket/Drawing.cs
@@ -6,25 +6,23 @@ public class Drawing : MonoBehaviour
 {
     public static bool startDraw;
     public static int randomNumber;
-    public List<GameObject> middleListOfObjects = new List<GameObject>();
-    public List<GameObject> endListOfObjects = new List<GameObject>();
-    public List<GameObject> listOfMohicans = new List<GameObject>();
+    public List<GameObject> middleListOfObjects = new List<GameObject>(), endListOfObjects = new List<GameObject>(), listOfLastObjects = new List<GameObject>();
     public GameObject[] gameObjectsArray, numbersArray;
-    public GameObject questionMarkFirstPrefab, questionMarkSecondPrefab, questionMarkFirst, questionMarkSecond, lastMohican;
+    public GameObject questionMarkFirstPrefab, questionMarkSecondPrefab, questionMarkFirst, questionMarkSecond, lastObject;
     public Transform destroyPositionFirst, instantiatePositionFirst, middlePositionFirst, destroyPositionSecnond, instantiatePositionSecond, middlePositionSecond;
     public TextMeshProUGUI endedDrawing;
     private GameObject[] mainArray;
     private GameObject questionMark;
     private Transform destroyPosition, middlePosition, instantiatePosition;
-    private float mainVelocity, parttimeToEndHurtAnimationVelocity;
+    private float mainVelocity, partTimeVelocity;
     private int i, j, k;
     private bool firstMove, nextMove, nextSitting = true, nextRound = true, infoIsEnabled, startDrawing;
     private readonly float firstVelocity = 15f, secondVelocity = 12f, thirdVelocity = 9f, fourthVelocity = 6f, fifthVelocity = 3f, endVelocity = 1f;
     private void Update()
     {
-        if (lastMohican != null && lastMohican.transform.position.y > middlePosition.position.y)
+        if (lastObject != null && lastObject.transform.position.y > middlePosition.position.y)
         {
-            lastMohican.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
+            lastObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
             k++;
             i = j = 0;
             mainVelocity = 15f;
@@ -32,8 +30,8 @@ public class Drawing : MonoBehaviour
             middleListOfObjects.Clear();
             endListOfObjects.Clear();
             Drawing.startDraw = nextSitting = nextRound = true;
-            listOfMohicans.Add(lastMohican);
-            lastMohican = questionMark = null;
+            listOfLastObjects.Add(lastObject);
+            lastObject = questionMark = null;
         }
         if (Drawing.startDraw & k < 2)
         {
@@ -65,11 +63,11 @@ public class Drawing : MonoBehaviour
                 }
                 nextRound = false;
             }
-            if (lastMohican != null && lastMohican.transform.position.y > middlePosition.position.y)
-                lastMohican.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
+            if (lastObject != null && lastObject.transform.position.y > middlePosition.position.y)
+                lastObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
             if (questionMark != null && questionMark.GetComponent<TextMeshProUGUI>().enabled & questionMark.transform.position.y > destroyPosition.position.y)
                 Destroy(questionMark);
-            nextMove = IsThisTrue(middleListOfObjects, endListOfObjects, middlePosition) ? false : nextMove;
+            nextMove = CheckingPosition(middleListOfObjects, endListOfObjects, middlePosition) ? false : nextMove;
             Destroy(TimeToDestroy(endListOfObjects, destroyPosition));
             TimeToDestroy(endListOfObjects, destroyPosition);
             if (!firstMove)
@@ -83,23 +81,23 @@ public class Drawing : MonoBehaviour
                     switch (j)
                     {
                         case 0:
-                            parttimeToEndHurtAnimationVelocity = Drawing.randomNumber == 0 ? (firstVelocity - secondVelocity) / 10 : (firstVelocity - secondVelocity) / Drawing.randomNumber;
+                            partTimeVelocity = Drawing.randomNumber == 0 ? (firstVelocity - secondVelocity) / 10 : (firstVelocity - secondVelocity) / Drawing.randomNumber;
                             nextSitting = false;
                             break;
                         case 1:
-                            parttimeToEndHurtAnimationVelocity = (secondVelocity - thirdVelocity) / 10;
+                            partTimeVelocity = (secondVelocity - thirdVelocity) / 10;
                             nextSitting = false;
                             break;
                         case 2:
-                            parttimeToEndHurtAnimationVelocity = (thirdVelocity - fourthVelocity) / 10;
+                            partTimeVelocity = (thirdVelocity - fourthVelocity) / 10;
                             nextSitting = false;
                             break;
                         case 3:
-                            parttimeToEndHurtAnimationVelocity = (fourthVelocity - fifthVelocity) / 10;
+                            partTimeVelocity = (fourthVelocity - fifthVelocity) / 10;
                             nextSitting = false;
                             break;
                         case 4:
-                            parttimeToEndHurtAnimationVelocity = (fifthVelocity - endVelocity) / 10;
+                            partTimeVelocity = (fifthVelocity - endVelocity) / 10;
                             nextSitting = false;
                             break;
                     }
@@ -110,65 +108,45 @@ public class Drawing : MonoBehaviour
                         j++;
                         nextSitting = true;
                     }
-                    GameObject newGameObject = Instantiate(mainArray[i], instantiatePosition.position, Quaternion.identity);
-                    newGameObject.transform.SetParent(GameObject.Find("DrawQuestionMarksCanvas").transform);
-                    AddToList(middleListOfObjects, newGameObject);
-                    newGameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, mainVelocity);
-                    mainVelocity -= parttimeToEndHurtAnimationVelocity;
+                    AddNewObject(mainArray, i, instantiatePosition, middleListOfObjects, mainVelocity);
+                    mainVelocity -= partTimeVelocity;
                     i++;
                     i = i > 9 ? 0 : i;
                 }
                 else if (j == 5 & i != Drawing.randomNumber)
                 {
-                    GameObject newGameObject = Instantiate(mainArray[i], instantiatePosition.position, Quaternion.identity);
-                    newGameObject.transform.SetParent(GameObject.Find("DrawQuestionMarksCanvas").transform);
-                    AddToList(middleListOfObjects, newGameObject);
-                    newGameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, mainVelocity);
-                    mainVelocity -= parttimeToEndHurtAnimationVelocity;
+                    AddNewObject(mainArray, i, instantiatePosition, middleListOfObjects, mainVelocity);
+                    mainVelocity -= partTimeVelocity;
                     i++;
                     i = i > 9 ? 0 : i;
                 }
                 else if (j == 5 & i == Drawing.randomNumber)
                 {
-                    GameObject newGameObject = Instantiate(mainArray[i], instantiatePosition.position, Quaternion.identity);
-                    newGameObject.transform.SetParent(GameObject.Find("DrawQuestionMarksCanvas").transform);
-                    AddToList(middleListOfObjects, newGameObject);
-                    newGameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, mainVelocity);
-                    lastMohican = newGameObject;
+                    lastObject = AddLastObject(mainArray, i, instantiatePosition, middleListOfObjects, mainVelocity);
                     j++;
                 }
                 nextMove = true;
             }
         }
         else if (Drawing.startDraw & k == 2)
-        {
-            GameObject questionGameObject = Instantiate(questionMarkFirstPrefab, new Vector3(middlePositionFirst.position.x + 0.032f, middlePositionFirst.position.y, 0f), Quaternion.identity);
-            questionMarkFirst = questionGameObject;
-            questionGameObject.transform.SetParent(GameObject.Find("DrawQuestionMarksCanvas").transform);
-            questionGameObject.GetComponent<RectTransform>().position = new Vector3(questionGameObject.GetComponent<RectTransform>().position.x, questionGameObject.GetComponent<RectTransform>().position.y, 7f);
-            questionGameObject.GetComponent<RectTransform>().localScale = new Vector3(1f, 1f, 1f);
-            questionGameObject.SetActive(false);
-            questionGameObject = Instantiate(questionMarkSecondPrefab, new Vector3(middlePositionSecond.position.x + 0.032f, middlePositionSecond.position.y, 0f), Quaternion.identity);
-            questionMarkSecond = questionGameObject;
-            questionGameObject.transform.SetParent(GameObject.Find("DrawQuestionMarksCanvas").transform);
-            questionGameObject.GetComponent<RectTransform>().position = new Vector3(questionGameObject.GetComponent<RectTransform>().position.x, questionGameObject.GetComponent<RectTransform>().position.y, 7f);
-            questionGameObject.GetComponent<RectTransform>().localScale = new Vector3(1f, 1f, 1f);
-            questionGameObject.SetActive(false);
+        {     
+            questionMarkFirst = AddQuestionMark(questionMarkFirstPrefab, middlePositionFirst);
+            questionMarkSecond = AddQuestionMark(questionMarkSecondPrefab, middlePositionSecond);
             infoIsEnabled = endedDrawing.enabled = true;
             Drawing.startDraw = false;
         }
         if (!endedDrawing.enabled & infoIsEnabled)
         {
-            foreach (GameObject gameObject in listOfMohicans)
+            foreach (GameObject gameObject in listOfLastObjects)
                 Destroy(gameObject);
-            listOfMohicans.Clear();
+            listOfLastObjects.Clear();
             questionMarkFirst.SetActive(true);
             questionMarkSecond.SetActive(true);
             i = j = k = 0;
             infoIsEnabled = startDrawing = false;
         }
     }
-    private bool IsThisTrue(List<GameObject> middleListOfObjects, List<GameObject> endListOfObjects, Transform middlePosition)
+    private bool CheckingPosition(List<GameObject> middleListOfObjects, List<GameObject> endListOfObjects, Transform middlePosition)
     {
         foreach (GameObject gameObject in middleListOfObjects)
             if (gameObject.transform.position.y > middlePosition.position.y)
@@ -198,6 +176,30 @@ public class Drawing : MonoBehaviour
         list.Add(gameObject);
         foreach (GameObject workGameObject in workList)
             list.Add(workGameObject);
+    }
+    private void AddNewObject(GameObject[] mainArray, int i, Transform instantiatePosition, List<GameObject> middleListOfObjects, float mainVelocity)
+    {
+        GameObject newGameObject = Instantiate(mainArray[i], instantiatePosition.position, Quaternion.identity);
+        newGameObject.transform.SetParent(GameObject.Find("DrawQuestionMarksCanvas").transform);
+        AddToList(middleListOfObjects, newGameObject);
+        newGameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, mainVelocity);
+    }
+    private GameObject AddLastObject(GameObject[] mainArray, int i, Transform instantiatePosition, List<GameObject> middleListOfObjects, float mainVelocity)
+    {
+        GameObject newGameObject = Instantiate(mainArray[i], instantiatePosition.position, Quaternion.identity);
+        newGameObject.transform.SetParent(GameObject.Find("DrawQuestionMarksCanvas").transform);
+        AddToList(middleListOfObjects, newGameObject);
+        newGameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, mainVelocity);
+        return newGameObject;
+    }
+    private GameObject AddQuestionMark(GameObject questionMarkPrefab,Transform position) 
+    {
+        GameObject questionGameObject = Instantiate(questionMarkPrefab, new Vector3(position.position.x + 0.032f, position.position.y, 0f), Quaternion.identity);
+        questionGameObject.transform.SetParent(GameObject.Find("DrawQuestionMarksCanvas").transform);
+        questionGameObject.GetComponent<RectTransform>().position = new Vector3(questionGameObject.GetComponent<RectTransform>().position.x, questionGameObject.GetComponent<RectTransform>().position.y, 7f);
+        questionGameObject.GetComponent<RectTransform>().localScale = new Vector3(1f, 1f, 1f);
+        questionGameObject.SetActive(false);
+        return questionGameObject;
     }
 
 }
